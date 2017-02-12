bde9b8007b7f23bf03e0523fac6764f34b2f75c5
Charlie Crisp <Charlie Crisp>
2017-02-12 17:18:01 +0000

    Added more tests

--- src/test/java/HttpParamTest.java
+++ src/test/java/HttpParamTest.java
@@ -8,10 +8,20 @@ import java.io.IOException;
 public class HttpParamTest {
     @Test public void httpParamTest() {
         try {
-            HashMap<String, String> testMap = (HashMap<String,String>) HttpParameters.parse("param1=val1&param2=val2");
-            assertEquals(testMap.get("param1"), "val1");
-            assertEquals(testMap.get("param2"), "val2");
+            //runs the parse method with the sample string
+            HashMap<String, String> testMap1 = (HashMap<String,String>) HttpParameters.parse("param1=val1&param2=val2");
+            //asserts correct keys map to correct values.
+            assertEquals(testMap1.get("param1"), "val1");
+            assertEquals(testMap1.get("param2"), "val2");
+            //sanity check
+            assertNotEquals(testMap1.get("param1"), "val");
+
+            //testing use of URL Decoder
+            HashMap<String, String> testMap2 = (HashMap<String,String>) HttpParameters.parse("param1=val1+val2&param2=val2");
+            assertEquals(testMap2.get("param1"), "val1 val2");
+
         } catch (IOException e) {
+            //the test will fail if there is a parsing error from HttpParameters class
             fail(e.getMessage());
         }
     }
